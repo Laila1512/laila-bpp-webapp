a9e59a106546f6a09339b1e45215ffa1152f257c
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-08-29 13:27:37 +0100

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,4 +1,13 @@
 document.write('<div>\
-    ... your navigation content ...\
+    <div class="topnav" id="myTopnav">
+  <a href=""><img src="Screenshot 2022-08-29 at 12.55.07.png" alt="EY logo" height="50px" width="50px"></a>
+  <a href="" class="active">Home</a>
+  <a href="about">About</a>
+  <a href="onboarding">Onboarding</a>
+  <a href="faqs">FAQs</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>\
     </div>\
 ');
