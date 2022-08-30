a2c2365f5d1f81c07bbfcb1506a65ecab0d31157
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-08-30 11:24:40 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,12 @@
 const jobroles =["HR", "Marketing", "Tax", "Assurance", "People Advisory Services"]
+// Get dropdown element from DOM
+var dropdown = document.getElementById("jobrole");
+
+// Loop through the array
+for (var i = 0; i < jobroles.length; ++i) {
+    // Append the element to the end of Array list
+    dropdown[dropdown.length] = new Option(jobroles[i], jobroles[i]);
+}
 
 function onSave() {
   let firstName = document.getElementById("fname").value;
