ae5ebcb59cb014c9d935f88342a5018ec00bf030
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-08-30 15:04:05 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,16 @@
-const jobroles =["HR", "Marketing", "Tax", "Assurance", "People Advisory Services"]
-var options = '';
+const joblist =["HR", "Marketing", "Tax", "Assurance", "People Advisory Services"];
+const datalist = document.getElementById("jobrole");
 
-for (var i = 0; i < jobroles.length; i++) {
-  options += '<option value="' + jobroles[i] + '" />';
-}
+for (const x of joblist) {
+  const option = document.createElement('option');
+  option.value = x;
+  datalist.appendChild(option);
+};
 
 let firstName = "";
 let lastName = "";
 let jobRole = "";
 let startDate = "";
-
-document.getElementById('jobrole').appendChild(options);
 
 function onSave() {
   firstName = document.getElementById("fname").value;
