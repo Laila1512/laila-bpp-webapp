d76c1c95e35fdaca9a4383ba15d2a59bd7d8700e
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-08-30 11:21:32 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,5 @@
+const jobroles =["HR", "Marketing", "Tax", "Assurance", "People Advisory Services"]
+
 function onSave() {
   let firstName = document.getElementById("fname").value;
   console.log(firstName, "test")
