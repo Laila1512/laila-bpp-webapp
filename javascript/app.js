616dcdc6621219ceacb2209ea94ff10dd847ac95
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-08-30 16:09:02 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,19 @@
+function onLoad() {
+const sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt";
+document.getElementById("sampleText").innerHTML = sampleText;
+  
+const joblist = ["HR", "Marketing", "Tax", "Assurance", "People Advisory Services"];
+let datalist = document.getElementById("jobrole");
+
+for (const x of joblist) {
+  const option = document.createElement('option');
+  option.value = x;
+  datalist.appendChild(option);
+};
+  
+console.log(joblist);
+}
+
 function onSave() {
   let firstName = "";
   let lastName = "";
