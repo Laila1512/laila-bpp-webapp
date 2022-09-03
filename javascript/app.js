900c8200ee03f30ffefbc44c57f7f182c47f9c41
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-09-03 15:55:04 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -23,7 +23,7 @@ function onSave() {
   let startDate = document.getElementById("startdate").value;
   
   if (validateFormData(firstName, lastName, email, jobRole, startDate)) {
-    document.getElementById("displayUserDetails").innerHTML = "Welcome " + firstName + " " + lastName + "<br>" + "Email: " +  email + "<br>" + jobRole + startDate;
+    document.getElementById("displayUserDetails").innerHTML = "<h1>Welcome " + firstName + " " + lastName + "</h1><br>" + "Email: " +  email + "<br>" + "Job role : " + jobRole + " start date: " + startDate;
   }
 }
 
