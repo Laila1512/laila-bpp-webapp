029fb6638e0d9e9e02ead59e2359905c6f9b1e75
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-09-03 20:36:47 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -14,6 +14,22 @@ for (const x of Object.values(joblist)) {
   option.value = x;
   datalist.appendChild(option);
 };
+ 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+   var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+     content.style.display = "none";
+   } else {
+     content.style.display = "block";
+   }
+  });
+}
+
 }
 
 function onSave() {
