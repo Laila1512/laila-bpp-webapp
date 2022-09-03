e1811119260327837efea4fa30eef82fa22486f2
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-09-03 19:16:27 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -64,3 +64,20 @@ function validateFormData() {
   }
   return validData;
 }
+
+function expandText() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
