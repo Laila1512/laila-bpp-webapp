4ff8fc368089113be9293a5ba7bf0ae5f84c148f
Laila1512 <74360881+Laila1512@users.noreply.github.com>
2022-09-03 20:39:34 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -21,12 +21,12 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-   var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-     content.style.display = "none";
-   } else {
-     content.style.display = "block";
-   }
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
   });
 }
 
